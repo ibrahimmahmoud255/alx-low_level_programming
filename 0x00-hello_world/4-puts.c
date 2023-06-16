e3663e05d9fe0037621e9aab93919d9f553fd9c8
ibrahimmahmoud255 <ibrahimkarizma82@gmail.com>
2023-06-15 19:07:06 -0700

    Commit MSG

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Description: A C program that prints with put function
- *
- * returnalways = 0 (Success)
-*/
-
-int main (void)
+ * main - main block
+ * Return: 0
+ */
+int main(void)
 {
-   puts ("\"Programming is like building a multilingual puzzle");
-   return = 0  
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
