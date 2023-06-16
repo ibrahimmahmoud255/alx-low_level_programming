84521c715612c0a7f5ee04b1aaa416466b6ec732
ibrahimmahmoud255 <ibrahimkarizma82@gmail.com>
2023-06-15 19:17:43 -0700

    Commit MSG

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Description: A C program that prints with printf function
- *
- * Return: always = 0 (Success)
-*/
-
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
-     printf("with proper grammar, but the outcome is a piece of art,\n");
-
-     return = (0);
-}	
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
